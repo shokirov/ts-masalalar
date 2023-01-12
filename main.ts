68f0ec375506814ab9ef360699b1387fd2e82534
ortiqovdev <akmalbekortiqov7976@gmail.com>
2023-01-12 12:28:09 +0500

    array 1-10

---
+++ main.ts
@@ -0,0 +1,76 @@
+// import { log } from "console";
+// import PromptSync from "prompt-sync";
+// const prompt = PromptSync();
+
+// ? Array1
+// let n: number = +prompt("n= ");
+// let q = 1
+// let y: number[] = new Array(n)
+// for (let i = 0; i < n; i++) {
+//     y[i] = q
+//     q += 2
+// }
+// console.log(y);
+
+// ?Array2
+// let n:number = 10
+// let x:Array<number>=new Array (n) 
+// for (let i=2 ; i<n ; i++){
+//     console.log(n*i);
+
+// }
+
+
+// ? Array5
+// let n:number =20
+// let fibbonaci : Array<number> =new Array(n)
+// fibbonaci[0]=1
+// fibbonaci[1]=1
+// for (let i=2; i<n ; i++){
+//     fibbonaci[i]=fibbonaci[i-1] + fibbonaci[i-2]
+// }
+// console.log(fibbonaci);
+
+// ?Array6
+// let n: number = 5
+// let A: number = 3
+// let B: number = 2
+// let x: Array<number> = new Array(n)
+// x[0] = A
+// x[1] = B
+// x[2] = x[0]+x[1]
+// for (let i=3 ; i<n ; i++){
+//     x[i] = x[i-1] *2
+// }
+// console.log(x);
+
+// ?? Array6 (2-usul)
+// let n: number = 10
+// let A: number = 3
+// let B: number = 2
+// let maxFibo: Array<number> = new Array()
+// maxFibo.push(A)
+// maxFibo.push(B)
+// maxFibo.push(A + B)
+// for (let i = 3; i < n; i++) {
+//     maxFibo.push(maxFibo[i - 1] * 2)
+// }
+// maxFibo
+
+//? Array10
+// let n: Array<number> = new Array( 12, 65, 87, 43, 23, 45, 90)
+// let randomNumber = n.length
+// for (let i= ; )
+
+
+// ? Array18
+// let randomNumber2: number[] = [72, 23, 123, 65, 76, 19, 62]
+// let len :number = randomNumber2.length
+
+// for (let i=0 ; i<randomNumber2.length; i++){
+//     if(randomNumber2[i] < randomNumber2[len -1 ]){
+//         console.log(randomNumber2[i]);
+//         break
+//     }
+// }
+
